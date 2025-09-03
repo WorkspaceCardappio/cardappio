fb8393db779a82581d9067352ea5f6364b669257
Cassia <125088421+cassiab13@users.noreply.github.com>
2025-09-02 22:19:11 -0300

    fix: remove unused import

--- server/cardappio/src/main/java/br/com/cardappio/DTO/TableRestaurantDTO.java
+++ server/cardappio/src/main/java/br/com/cardappio/DTO/TableRestaurantDTO.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.UniqueElements;
 
 import java.util.UUID;
 
