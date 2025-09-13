2acee99467ac4d1c9fc48d97d784a898bff6887f
Cassia <125088421+cassiab13@users.noreply.github.com>
2025-09-13 15:21:52 -0300

    CP-229 fix: routes

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: 'category',
-        loadComponent: () => import('./components/category/category.component').then(m => m.CategoryComponent)
+        loadComponent: () => import('./pages/category/category.component').then(m => m.CategoryComponent)
     }
 ];
