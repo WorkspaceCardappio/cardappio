6ba937c41f379da8bac6ef0315be007967511294
jean.soares <jean.soares@elotech.com.br>
2025-09-12 23:37:48 -0300

    fix: subindo versao e removendo componentes desnecessarios

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
 
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
