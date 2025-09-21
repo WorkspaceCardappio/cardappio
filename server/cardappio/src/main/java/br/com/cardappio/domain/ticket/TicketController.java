99f74c9183568a27931fcc20a08ad86b3a05e142
jean.soares <jean.soares@elotech.com.br>
2025-09-21 09:19:10 -0300

    fix: passando list

--- server/cardappio/src/main/java/br/com/cardappio/domain/ticket/TicketController.java
+++ server/cardappio/src/main/java/br/com/cardappio/domain/ticket/TicketController.java
@@ -8,8 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cardappio.core.controller.CrudController;
 
 import br.com.cardappio.domain.ticket.dto.TicketDTO;
+import br.com.cardappio.domain.ticket.dto.TicketListDTO;
 
 @RestController
 @RequestMapping("/tickets")
-public class TicketController extends CrudController<Ticket, UUID, TicketDTO, TicketDTO> {
+public class TicketController extends CrudController<Ticket, UUID, TicketListDTO, TicketDTO> {
 }
