ee522b78603d31893df83644beb6b4b6653e007d
Cassia <125088421+cassiab13@users.noreply.github.com>
2025-10-03 21:49:48 -0300

    refactor: ingredient DTO

--- server/cardappio/src/main/java/br/com/cardappio/domain/ingredient/enums/UnityOfMeasurement.java
+++ server/cardappio/src/main/java/br/com/cardappio/domain/ingredient/enums/UnityOfMeasurement.java
@@ -1,11 +1,27 @@
 package br.com.cardappio.domain.ingredient.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum UnityOfMeasurement {
-    LITER,
-    MILLILITER,
-    GRAM,
-    KILOGRAM,
-    UNIT
+    LITER(0, "Litro"),
+    MILLILITER(1, "Mililitro"),
+    GRAM(2, "Grama"),
+    KILOGRAM(3, "Quilograma"),
+    UNIT(4, "Unidade");
 
+    private final int code;
+    private final String description;
 
+    public static UnityOfMeasurement fromCode(int code){
+        for(UnityOfMeasurement value: UnityOfMeasurement.values()){
+            if(value.code == code){
+                return value;
+            }
+        } throw new IllegalArgumentException("Código inválido" + code);
+    }
 }
+
+
