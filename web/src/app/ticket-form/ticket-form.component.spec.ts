ce10511e60f62ef454525af45efe1aa3975783aa
Ricardo Filho <ricardo-tagami@hotmail.com>
2025-10-06 20:29:46 -0300

    test: fix tests front

--- web/src/app/ticket-form/ticket-form.component.spec.ts
+++ web/src/app/ticket-form/ticket-form.component.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TicketFormComponent } from './ticket-form.component';
+import { ActivatedRoute, provideRouter } from "@angular/router";
+import { provideHttpClient } from "@angular/common/http";
 
-describe('TicketFormComponent', () => {
+fdescribe('TicketFormComponent', () => {
   let component: TicketFormComponent;
   let fixture: ComponentFixture<TicketFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TicketFormComponent]
+      imports: [TicketFormComponent],
+      providers: [
+        provideHttpClient(),
+        provideRouter([]),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: { id: 'new' }
+            }
+          }
+        }
+      ]
     })
     .compileComponents();
 
