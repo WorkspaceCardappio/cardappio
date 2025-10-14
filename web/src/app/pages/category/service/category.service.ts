9cb7c294762f472a9a78706df433991ca2af0427
jean.soares <jean.soares@elotech.com.br>
2025-10-14 10:58:04 -0300

    feat: salvando categoria

--- web/src/app/pages/category/service/category.service.ts
+++ web/src/app/pages/category/service/category.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { EntityService } from '../../../entity-service/entity.service';
-import { Category } from '../model/category';
+import { Category } from '../../../model/category';
 
 @Injectable({
   providedIn: 'root'
