2582b50bf47d0ac9c255783ab0ffea411ad4dd4c
Vinicius Vilela <viniciusvilela10@gmail.com>
2025-10-16 22:09:41 -0300

    fix test

--- server/cardappio/src/main/resources/db/migration/V202509182136__product_order.sql
+++ server/cardappio/src/main/resources/db/migration/V202509182136__product_order.sql
@@ -1,12 +1,12 @@
---CREATE TABLE IF NOT EXISTS product_order (
---    id UUID PRIMARY KEY,
---    client_order_id UUID NOT NULL,
---    product_id UUID NOT NULL,
---    price DECIMAL(10,2) NOT NULL,
---    quantity DECIMAL(10,2) NOT NULL,
---    total DECIMAL(10,2) NOT NULL,
---    note VARCHAR(255),
---
---    CONSTRAINT fk_product_order_client_order_id FOREIGN KEY (client_order_id) REFERENCES client_order(id),
---    CONSTRAINT fk_product_order_product_id FOREIGN KEY (product_id) REFERENCES product(id)
---);
+CREATE TABLE IF NOT EXISTS product_order (
+   id UUID PRIMARY KEY,
+   client_order_id UUID NOT NULL,
+   product_id UUID NOT NULL,
+   price DECIMAL(10,2) NOT NULL,
+   quantity DECIMAL(10,2) NOT NULL,
+   total DECIMAL(10,2) NOT NULL,
+   note VARCHAR(255),
+
+   CONSTRAINT fk_product_order_client_order_id FOREIGN KEY (client_order_id) REFERENCES client_order(id),
+   CONSTRAINT fk_product_order_product_id FOREIGN KEY (product_id) REFERENCES product(id)
+);
