94523a6676523a7c5862d3da2fb4e56d367afa02
Vinicius Vilela <viniciusvilela10@gmail.com>
2025-10-16 22:34:38 -0300

    fix test

--- server/cardappio/src/main/java/br/com/cardappio/domain/product/dto/ProductDTO.java
+++ server/cardappio/src/main/java/br/com/cardappio/domain/product/dto/ProductDTO.java
@@ -1,22 +1,20 @@
 package br.com.cardappio.domain.product.dto;
 
-import br.com.cardappio.domain.additional.dto.AdditionalDTO;
-import br.com.cardappio.domain.category.Category;
-import br.com.cardappio.domain.ingredient.Ingredient;
-import br.com.cardappio.domain.ingredient.dto.IngredientDTO;
-import br.com.cardappio.domain.product.Product;
-import br.com.cardappio.utils.Messages;
-
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.Length;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.cardappio.domain.additional.dto.AdditionalDTO;
+import br.com.cardappio.domain.category.Category;
+import br.com.cardappio.domain.product.Product;
+import br.com.cardappio.utils.Messages;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record ProductDTO(
 
@@ -43,16 +41,15 @@ public record ProductDTO(
         String image,
 
         @NotNull
-        Category category
-        
+        Category category,
+
         List<AdditionalDTO> additional,
 
         List<ProductVariableDTO> variables,
 
         List<ProductIngredientDTO> ingredients
 ) {
-
-    public ProductDTO(final Product product){
+    public ProductDTO(final Product product) {
         this(
                 product.getId(),
                 product.getName(),
@@ -61,9 +58,10 @@ public record ProductDTO(
                 product.getActive(),
                 product.getExpirationDate(),
                 product.getImage(),
-                product.getCategory()
+                product.getCategory(),
+                List.of(),
+                List.of(),
+                List.of()
         );
     }
 }
-
-) {}
