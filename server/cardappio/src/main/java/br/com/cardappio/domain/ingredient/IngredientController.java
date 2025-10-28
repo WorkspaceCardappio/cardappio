9396bc9b159971fc090ff7dad023616816d1ba0e
jean.soares <jean.soares@elotech.com.br>
2025-10-27 21:11:50 -0300

    fix: ingredient na listagem

--- server/cardappio/src/main/java/br/com/cardappio/domain/ingredient/IngredientController.java
+++ server/cardappio/src/main/java/br/com/cardappio/domain/ingredient/IngredientController.java
@@ -1,16 +1,17 @@
 package br.com.cardappio.domain.ingredient;
 
-import br.com.cardappio.domain.ingredient.dto.IngredientDTO;
+import java.util.List;
+import java.util.UUID;
 
-import br.com.cardappio.domain.ingredient.dto.IngredientListDTO;
-import br.com.cardappio.domain.ingredient.dto.UnityOfMeasurementDTO;
-import com.cardappio.core.controller.CrudController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.UUID;
+import com.cardappio.core.controller.CrudController;
+
+import br.com.cardappio.domain.ingredient.dto.IngredientDTO;
+import br.com.cardappio.domain.ingredient.dto.IngredientListDTO;
+import br.com.cardappio.domain.ingredient.dto.UnityOfMeasurementDTO;
 
 @RestController
 @RequestMapping("/api/ingredients")
@@ -18,11 +19,12 @@ public class IngredientController extends CrudController<Ingredient, UUID, Ingre
 
     private final IngredientService ingredientService;
 
-    public IngredientController(final IngredientService ingredientService){
+    public IngredientController(final IngredientService ingredientService) {
         this.ingredientService = ingredientService;
     }
+
     @GetMapping("/unity-of-measurement")
-    public List<UnityOfMeasurementDTO> getUnitOfMeasurement(){
+    public List<UnityOfMeasurementDTO> getUnitOfMeasurement() {
         return ingredientService.getUnityOfMeasurement();
     }
 
