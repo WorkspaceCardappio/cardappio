5d8afb88192cec4dae02f1bd1b14d31aab955680
Ricardo Tagami <ricardo-tagami@hotmail.com>
2025-11-07 22:39:28 -0300

    fix: corrigindo pipe

--- server/cardappio/src/main/java/br/com/cardappio/domain/ticket/split/SplitService.java
+++ server/cardappio/src/main/java/br/com/cardappio/domain/ticket/split/SplitService.java
@@ -1,16 +1,5 @@
 package br.com.cardappio.domain.ticket.split;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.UUID;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import br.com.cardappio.domain.order.Order;
 import br.com.cardappio.domain.order.OrderRepository;
 import br.com.cardappio.domain.person.Person;
@@ -23,12 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
