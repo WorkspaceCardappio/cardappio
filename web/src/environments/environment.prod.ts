002479d8d4b5f9d6f5316e5cc7ed892bb5ff0ce8
Vinicius Vilela <viniciusvilela10@gmail.com>
2025-11-12 20:31:15 -0300

    build: env config

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://cardappio-prod.up.railway.app/api',
   keycloak: {
-    url: 'https://seu-keycloak.up.railway.app', // TODO: Alterar para a URL real do Keycloak no Railway
+    url: 'https://keycloak-cardappio.up.railway.app',
     realm: 'cardappio-app',
     clientId: 'frontend-app'
   }
