3a4f29e2661d5fb3dce31fe853356a57cef55816
jean.soares <jean.soares@elotech.com.br>
2025-11-12 20:58:47 -0300

    fix

--- server/cardappio/src/main/java/br/com/cardappio/domain/ingredient/IngredientController.java
+++ server/cardappio/src/main/java/br/com/cardappio/domain/ingredient/IngredientController.java
@@ -1,18 +1,18 @@
 package br.com.cardappio.domain.ingredient;
 
-import br.com.cardappio.domain.ingredient.dto.IngredientDTO;
-import br.com.cardappio.domain.ingredient.dto.IngredientListDTO;
-import br.com.cardappio.domain.ingredient.dto.UnityOfMeasurementDTO;
-import com.cardappio.core.controller.CrudController;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.UUID;
+import com.cardappio.core.controller.CrudController;
 
-@CrossOrigin(origins = "http://localhost:4200")
+import br.com.cardappio.domain.ingredient.dto.IngredientDTO;
+import br.com.cardappio.domain.ingredient.dto.IngredientListDTO;
+import br.com.cardappio.domain.ingredient.dto.UnityOfMeasurementDTO;
+
 @RestController
 @RequestMapping("/api/ingredients")
 public class IngredientController extends CrudController<Ingredient, UUID, IngredientListDTO, IngredientDTO> {
--- server/cardappio/src/main/java/br/com/cardappio/domain/order/additional/OrderAdditionalController.java
+++ server/cardappio/src/main/java/br/com/cardappio/domain/order/additional/OrderAdditionalController.java
@@ -1,17 +1,21 @@
 package br.com.cardappio.domain.order.additional;
 
-import br.com.cardappio.domain.order.ProductOrderAdditional;
-import br.com.cardappio.domain.order.additional.dto.OrderAdditionalDTO;
-import com.cardappio.core.controller.CrudController;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin(origins = "http://localhost:4200")
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cardappio.core.controller.CrudController;
+
+import br.com.cardappio.domain.order.ProductOrderAdditional;
+import br.com.cardappio.domain.order.additional.dto.OrderAdditionalDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/order-additionals")
 @RequiredArgsConstructor
