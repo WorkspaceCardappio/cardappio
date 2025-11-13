a4b251414d6d81f940ca30b50c7af739e9c00f61
jean.soares <jean.soares@elotech.com.br>
2025-11-12 21:02:30 -0300

    remove allow credentials

--- server/cardappio/src/main/java/br/com/cardappio/config/CorsConfig.java
+++ server/cardappio/src/main/java/br/com/cardappio/config/CorsConfig.java
@@ -1,13 +1,14 @@
 package br.com.cardappio.config;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,8 +22,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(Optional.ofNullable(corsProperties.getAllowedOrigins()).orElse(List.of()).toArray(new String[0]))
                 .allowedMethods(Optional.ofNullable(corsProperties.getAllowedMethods()).orElse(List.of()).toArray(new String[0]))
-                .allowedHeaders(Optional.ofNullable(corsProperties.getAllowedHeaders()).orElse(List.of()).toArray(new String[0]))
-                .allowCredentials(true);
+                .allowedHeaders(Optional.ofNullable(corsProperties.getAllowedHeaders()).orElse(List.of()).toArray(new String[0]));
     }
 
 }
