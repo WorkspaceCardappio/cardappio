0f78f464a23257b3af9fcaecd754414eb07588f8
Vinicius Vilela <viniciusvilela10@gmail.com>
2025-11-19 13:59:40 -0300

    feat: websocket gráficos

--- web/src/app/pages/order/list/order-list.component.spec.ts
+++ web/src/app/pages/order/list/order-list.component.spec.ts
@@ -4,10 +4,19 @@ import { provideHttpClient } from "@angular/common/http";
 import { provideHttpClientTesting } from "@angular/common/http/testing";
 import { provideRouter } from '@angular/router';
 import { OrderListComponent } from "./order-list.component";
+import { WebSocketService } from '../../../core/services/websocket.service';
+import { of } from 'rxjs';
 
 describe('OrderListComponent', () => {
   let component: OrderListComponent;
   let fixture: ComponentFixture<OrderListComponent>;
+
+  const mockWebSocketService = {
+    getOrderEvents: () => of(null),
+    getTicketEvents: () => of(null),
+    getConnectionStatus: () => of(false),
+    disconnect: () => {}
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,6 +25,7 @@ describe('OrderListComponent', () => {
         provideHttpClient(),
         provideRouter([]),
         provideHttpClientTesting(),
+        { provide: WebSocketService, useValue: mockWebSocketService }
       ]
     })
       .compileComponents();
--- web/src/app/pages/ticket/list/ticket-list.component.spec.ts
+++ web/src/app/pages/ticket/list/ticket-list.component.spec.ts
@@ -4,15 +4,29 @@ import { provideHttpClient } from "@angular/common/http";
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { TicketListComponent } from './ticket-list.component';
+import { WebSocketService } from '../../../core/services/websocket.service';
+import { of } from 'rxjs';
 
 describe('TicketListComponent', () => {
   let component: TicketListComponent;
   let fixture: ComponentFixture<TicketListComponent>;
 
+  const mockWebSocketService = {
+    getOrderEvents: () => of(null),
+    getTicketEvents: () => of(null),
+    getConnectionStatus: () => of(false),
+    disconnect: () => {}
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TicketListComponent],
-      providers: [provideHttpClient(), provideRouter([]), provideNoopAnimations()]
+      providers: [
+        provideHttpClient(),
+        provideRouter([]),
+        provideNoopAnimations(),
+        { provide: WebSocketService, useValue: mockWebSocketService }
+      ]
     })
     .compileComponents();
 
